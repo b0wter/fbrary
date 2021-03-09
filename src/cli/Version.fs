8dbb1d7ef3a93fbbf1e7d48240dbdf85901d4b87
b0wter <b0wter@gmx.de>
2021-03-09 21:49:13 +0100

    Updated publish script to change Version.fs

--- src/cli/Version.fs
+++ src/cli/Version.fs
@@ -2,5 +2,6 @@ namespace b0wter.Fbrary
 
 module Version =
     
+    // This version is updated automatically by the `publish` command.
+    // The script only works if the version number consists of three numbers separated by dots.
     let current = "1.8.0"
-
