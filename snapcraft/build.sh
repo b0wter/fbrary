81f73b3831245d8da9ea2512aa5fe97aeeb80f36
b0wter <b0wter@gmx.de>
2021-04-07 11:15:27 +0200

    Added snap package.

---
+++ snapcraft/build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+rm -rf build
+cd ../src/cli
+dotnet publish -r linux-x64 --self-contained true -c Release 
+cp -r bin/Release/net5.0/linux-x64/publish/ ../../snapcraft/build
+cd -
+snapcraft
